4ea6464da4b24f35af341a80b78c0283419be53b
Tuan Huynh <huynhminhanhtuan@gmail.com>
2016-10-15 16:14:02 +0700

    fix email

--- gencoupon.php
+++ gencoupon.php
@@ -3,10 +3,12 @@ require_once("/wp-load.php");
 require_once("/wp-config.php");
 require_once 'swift/lib/swift_required.php';
 global $wpdb;
-$oldEmail = $wpdb->get_row( "SELECT email FROM face_coupon_email WHERE coupon='c31ab58abc'");
+$email = $_GET['email'];
+$oldEmail = $wpdb->get_row( $wpdb->prepare("SELECT * FROM face_coupon_email WHERE email = %s", $email));
 if (!is_null($oldEmail)) {
 	echo "fail";
 } else {
+	
 $coupon = generate_coupons15();
 $wpdb->insert( 
 	'face_coupon_email', 
